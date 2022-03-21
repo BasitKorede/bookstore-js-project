b2662b6db3fdc64355c2d0ef73ec1748a6c230d2
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-03-21 18:58:16 +0500

    updated js file

--- index.js
+++ index.js
@@ -36,4 +36,63 @@ const store = (initialData = []) => {
       remove,
     };
   };
-  
+  // Display book function:
+// 1. accepts an object with {id, author, title}
+// 2. creates a li element and populates the objects with it
+// 3. query the ul element and appends the li to it
+const displayBook = ({ title, author, id }, parentElement) => {
+  const bookListItemElement = document.createElement('li');
+  bookListItemElement.className = 'book-list-item';
+  bookListItemElement.innerHTML = `
+      <section>
+        <h3>${title}</h3>
+        <p>${author}</p>
+        <button id="${id}" type="button" onclick="handleRemove('${id}')" class="remove-button">Remove</button>
+        </section>`;
+  parentElement.appendChild(bookListItemElement);
+};
+
+const generateId = () => `id_${Math.random().toString(36).slice(2)}`;
+
+const initialBooks = [
+  {
+    title: 'the boy with wings',
+    author: 'Basit Korede',
+    id: generateId(),
+  },
+
+  {
+    title: 'Think Pythone',
+    author: 'korede Basit',
+    id: generateId(),
+  },
+];
+
+const bookListElement = document.querySelector('ul.book-list');
+
+const bookStore = store(initialBooks);
+const books = bookStore.all();
+books.forEach((book) => {
+  displayBook(book, bookListElement);
+});
+
+const formElement = document.querySelector('#book-form');
+const handleSubmition = (event) => {
+  event.preventDefault();
+  const title = document.querySelector('.title-input').value;
+  const author = document.querySelector('.author-input').value;
+  const id = generateId();
+  const newBook = { title, author, id };
+  if (bookStore.add(newBook)) {
+    displayBook(newBook, bookListElement);
+  }
+};
+formElement.addEventListener('submit', handleSubmition);
+
+// eslint-disable-next-line no-unused-vars
+const handleRemove = (currentId) => {
+  if (bookStore.remove(currentId)) {
+    const removeButton = document.getElementById(currentId);
+    removeButton.parentElement.parentElement.remove();
+  }
+};
