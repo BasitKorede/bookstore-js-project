6a9ef857fc2ce85588e15333afbce8992221c097
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-03-22 13:48:25 +0500

    updated js file

--- index.js
+++ index.js
@@ -6,51 +6,49 @@ class Book {
   }
 }
 
-const store = (initialData = []) => {
-  let books;
+class BookStore {
+  constructor(initialData = []) {
+    this.saveToLocalStorage = (data) => {
+      const booksString = JSON.stringify(data);
+      localStorage.setItem('bookStoreData', booksString);
+      return true;
+    };
 
-  const saveToLocalStorage = (data) => {
-    const booksString = JSON.stringify(data);
-    localStorage.setItem("bookStoreData", booksString);
-    return true;
-  };
-
-  const rawBooksData = localStorage.getItem("bookStoreData");
-  if (rawBooksData) {
-    books = JSON.parse(rawBooksData);
-  } else {
-    books = initialData;
-    saveToLocalStorage(books);
+    const rawBooksData = localStorage.getItem('bookStoreData');
+    if (rawBooksData) {
+      this.books = JSON.parse(rawBooksData);
+    } else {
+      this.books = initialData;
+      this.saveToLocalStorage(this.books);
+    }
   }
 
-  const all = () => books;
+  all() {
+    return this.books;
+  }
 
-  const add = (newData) => {
+  add(newData) {
     if (!newData || !newData.id) {
       return false;
     }
-    books.push(newData);
-    return saveToLocalStorage(books);
-  };
 
-  const remove = (id) => {
-    books = books.filter((book) => book.id !== id);
-    return saveToLocalStorage(books);
-  };
+    const newBook = new Book(newData);
+    this.books.push(newBook);
+    return this.saveToLocalStorage(this.books);
+  }
 
-  return {
-    all,
-    add,
-    remove,
-  };
-};
+  remove(id) {
+    this.books = this.books.filter((book) => book.id !== id);
+    return this.saveToLocalStorage(this.books);
+  }
+}
 // Display book function:
 // 1. accepts an object with {id, author, title}
 // 2. creates a li element and populates the objects with it
 // 3. query the ul element and appends the li to it
 const displayBook = ({ title, author, id }, parentElement) => {
-  const bookListItemElement = document.createElement("li");
-  bookListItemElement.className = "book-list-item";
+  const bookListItemElement = document.createElement('li');
+  bookListItemElement.className = 'book-list-item';
   bookListItemElement.innerHTML = `
   <section class="book-store-section display-flex">
   <div class="display-flex">
@@ -67,19 +65,19 @@ const generateId = () => `id_${Math.random().toString(36).slice(2)}`;
 
 const initialBooks = [
   {
-    title: "the boy with wings",
-    author: "Basit Korede",
+    title: 'the boy with wings',
+    author: 'Basit Korede',
     id: generateId(),
   },
 
   {
-    title: "Think Pythone",
-    author: "korede Basit",
+    title: 'Think Pythone',
+    author: 'korede Basit',
     id: generateId(),
   },
 ];
 
-const bookListElement = document.querySelector("ul.book-list");
+const bookListElement = document.querySelector('ul.book-list');
 
 const bookStore = store(initialBooks);
 const books = bookStore.all();
@@ -87,18 +85,18 @@ books.forEach((book) => {
   displayBook(book, bookListElement);
 });
 
-const formElement = document.querySelector("#book-form");
+const formElement = document.querySelector('#book-form');
 const handleSubmition = (event) => {
   event.preventDefault();
-  const title = document.querySelector(".title-input").value;
-  const author = document.querySelector(".author-input").value;
+  const title = document.querySelector('.title-input').value;
+  const author = document.querySelector('.author-input').value;
   const id = generateId();
   const newBook = new Book({ title, author, id });
   if (bookStore.add(newBook)) {
     displayBook(newBook, bookListElement);
   }
 };
-formElement.addEventListener("submit", handleSubmition);
+formElement.addEventListener('submit', handleSubmition);
 
 // eslint-disable-next-line no-unused-vars
 const handleRemove = (currentId) => {
